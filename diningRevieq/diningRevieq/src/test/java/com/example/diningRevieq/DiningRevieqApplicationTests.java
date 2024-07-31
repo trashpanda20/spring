8824168fe8b05a941fdea558d9f9c431a49f4d31
Sam <104803895+trashpanda20@users.noreply.github.com>
2024-07-31 14:18:46 -0500

    Add files via upload

---
+++ diningRevieq/diningRevieq/src/test/java/com/example/diningRevieq/DiningRevieqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diningRevieq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningRevieqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
